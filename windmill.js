9a05dd1bb8d6ba938e587526b41b3bad33a33fba
James-Ruane <JamesRuane01@gmail.com>
2023-12-16 21:29:26 +0000

    added_comments

--- windmill.js
+++ windmill.js
@@ -1,32 +1,39 @@
 export default class Windmill{
     constructor(x,y,z, d, h, w, bound){
+        //depth, width, height of windmill
         this.depth = d;
         this.height = h;
         this.width = w;
+        // position of centre of windmill
         this.x = x;
         this.y = y;
         this.z = z;
+        this.position = new THREE.Vector3(this.x,this.y,this.z);
+        // threeJS materials
         this.geometry = new THREE.BoxGeometry( this.depth, this.height, this.width);    
         this.material = new THREE.MeshBasicMaterial( { color: 0x000000 } ); 
         this.mesh = new THREE.Mesh( this.geometry, this.material );
-        this.position = new THREE.Vector3(this.x,this.y,this.z);
+        // gets points in windmill trajectory
         this.points = this.getPoints();
+
+        // gets max X, Y, Z or windmills. i.e. topLeft, topRight, bottomRight, bottomLeft corners
         this.minZ = this.position.z - this.width / 2;
         this.maxZ = this.position.z + this.width / 2;
 
-        this.TRX = 0
-        this.TRY = 0
+        this.TRX = 0;
+        this.TRY = 0;
         this.TLX = 0;
         this.TLY = 0;
 
-        this.BLX = 0
-        this.BLY = 0
+        this.BLX = 0;
+        this.BLY = 0;
         this.BRX = 0;
         this.BRY = 0;
 
         this.rotation = 1.5;
     }
 
+    // updates the windmills corners and rotates the blades 
     update(){
         const x = this.x;
         const y = this.y;
@@ -38,7 +45,7 @@ export default class Windmill{
         this.TLX = (x * Math.sin(this.rotation - Math.PI + 0.15)+x);
         this.TLY = (x * Math.cos(this.rotation - Math.PI + 0.15))+y;
         
-        this.BLX = (x * Math.sin(this.rotation - Math.PI)+x)
+        this.BLX = (x * Math.sin(this.rotation - Math.PI)+x);
         this.BLY = (x * Math.cos(this.rotation - Math.PI)+y);
         this.BRX = (x * Math.sin(this.rotation + 0.15)+x);
         this.BRY = (x * Math.cos(this.rotation + 0.15)+y);
@@ -47,12 +54,13 @@ export default class Windmill{
         this.rotation = this.rotation % (2 * Math.PI);
     }
 
+    // creates a list of points that are contained within the windmills trajerctory
     getPoints(){
         const radius = this.depth / 2;
         const TRX = (radius * Math.sin(Math.PI / 2)+this.x);
         const BLX = this.position.x;
         const maxZ = this.z + this.width / 2;
-        const minZ = this.z - this.width / 2
+        const minZ = this.z - this.width / 2;
         var sprCoef = 0;
         var points = [];
 
@@ -60,17 +68,18 @@ export default class Windmill{
             for (var i=BLX; i<TRX; i+=3){
                 sprCoef += 2 * Math.PI / (TRX - BLX);
                 for (var j=sprCoef; j<sprCoef + 2*Math.PI; j+= 0.1){
-                    points.push([((i - BLX) * Math.sin(j)+this.x), ((i - BLX) * Math.cos(j)+this.y), h])
+                    points.push([((i - BLX) * Math.sin(j)+this.x), ((i - BLX) * Math.cos(j)+this.y), h]);
                 }
             }
         }
         return points;
     }
 
+    // generates a random number between a given min and max value
     random(min, max){
-        const difference = max - min
-        const random = Math.round(difference * Math.random())
-        return random + min
+        const difference = max - min;
+        const random = Math.round(difference * Math.random());
+        return random + min;
     }
 }
 
